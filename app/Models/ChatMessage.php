a7f94ddc146674eeda56e0463bf6a6514165ef0d
kyawzinwin2002 <kyawzinw469@gmail.com>
2023-10-28 14:11:10 +0630

    prepareDatabase

---
+++ app/Models/ChatMessage.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ChatMessage extends Model
+{
+    use HasFactory;
+
+    public function room()
+    {
+        return $this->hasOne(ChatRoom::class);
+    }
+
+    public function user()
+    {
+        return $this->hasOne(User::class);
+    }
+}
